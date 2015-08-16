8df0e43a974a0f3331b6a9c5aafe33a4c386e2f7
John Boyes <john@edinburghwebservices.co.uk>
2015-08-16 11:54:30 +0100

    added success test by notifying on runscope

--- functional_test/functional_test.rb
+++ functional_test/functional_test.rb
@@ -9,6 +9,7 @@ class FunctionalTest < Minitest::Test
 
   def setup
     ENV.store 'SLACK_WEBHOOK', 'http://localhost:7001/responses/slack'
+    delete_all_success_notifications
     Mirage.start
     @mirage = Mirage::Client.new
     @mirage.put('slack', 'Notification received on Slack') { http_method 'POST' }
@@ -42,6 +43,18 @@ class FunctionalTest < Minitest::Test
     retryable { JSON.parse(@mirage.requests(1).body) }
   end
 
+  def success_notifications
+    runscope_messages = JSON.parse( RestClient.get "https://api.runscope.com/buckets/32dq2c18qk24/stream", {:Authorization => 'Bearer 05d16442-31df-4f23-947d-9a5eec4f0525'} )
+    assert_equal "success", runscope_messages["meta"]["status"]
+    runscope_messages["data"]
+  end
+
+  def delete_all_success_notifications
+    success_notifications.each do | message |
+      RestClient.delete "https://api.runscope.com/buckets/32dq2c18qk24/messages/#{message['uuid']}", {:Authorization => 'Bearer 05d16442-31df-4f23-947d-9a5eec4f0525'}
+    end
+  end
+
   def test_run_all_specs
     @monitor_thread = Thread.new { SyntheticMonitor.new.monitor ENV['SLACK_WEBHOOK'] }
     assert_notification_sent_to_slack
@@ -53,14 +66,23 @@ class FunctionalTest < Minitest::Test
   end
 
   def test_run_success_spec
-    @monitor_thread = Thread.new { SyntheticMonitor.new.monitor 'spec/success_spec.rb', ENV['SLACK_WEBHOOK'] }
+    @monitor_thread = Thread.new {
+      monitor = SyntheticMonitor.new(success_notifications_url: 'https://32dq2c18qk24.runscope.net')
+      monitor.monitor 'spec/success_spec.rb', ENV['SLACK_WEBHOOK']
+    }
     assert_no_notification_sent_to_slack
+    assert_equal 1, success_notifications.size
+    success_notification = JSON.parse( RestClient.get "https://api.runscope.com/buckets/32dq2c18qk24/messages/#{success_notifications.first['uuid']}", {:Authorization => 'Bearer 05d16442-31df-4f23-947d-9a5eec4f0525'} )
+    body = JSON.parse success_notification["data"]["request"]["body"]
+    assert_equal("all monitoring tests passed", body["data"])
   end
+
 
   def teardown
     @monitor_thread.exit
     @mirage.clear
     Mirage.stop
+    delete_all_success_notifications
   end
 
 end
