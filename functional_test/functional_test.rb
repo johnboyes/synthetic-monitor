bf285b49e96dbaa4cf665858b3103c1644f82098
John Boyes <john@edinburghwebservices.co.uk>
2015-08-16 12:50:11 +0100

    added assertion for when no success notification is sent

--- functional_test/functional_test.rb
+++ functional_test/functional_test.rb
@@ -35,6 +35,17 @@ class FunctionalTest < Minitest::Test
     runscope_messages["data"]
   end
 
+  def assert_success_notification_sent
+    assert_equal 1, success_notifications.size
+    success_notification = JSON.parse( RestClient.get "https://api.runscope.com/buckets/32dq2c18qk24/messages/#{success_notifications.first['uuid']}", {:Authorization => 'Bearer 05d16442-31df-4f23-947d-9a5eec4f0525'} )
+    body = JSON.parse success_notification["data"]["request"]["body"]
+    assert_equal("all monitoring tests passed", body["data"])
+  end
+
+  def assert_no_success_notification_sent
+    assert_empty success_notifications
+  end
+
   def delete_all_success_notifications
     success_notifications.each do | message |
       RestClient.delete "https://api.runscope.com/buckets/32dq2c18qk24/messages/#{message['uuid']}", {:Authorization => 'Bearer 05d16442-31df-4f23-947d-9a5eec4f0525'}
@@ -44,11 +55,13 @@ class FunctionalTest < Minitest::Test
   def test_run_all_specs
     @monitor_thread = Thread.new { SyntheticMonitor.new.monitor ENV['SLACK_WEBHOOK'] }
     assert_notification_sent_to_slack
+    assert_no_success_notification_sent
   end
 
   def test_run_failure_spec
     @monitor_thread = Thread.new { SyntheticMonitor.new.monitor 'spec/failure_spec.rb', ENV['SLACK_WEBHOOK'] }
     assert_notification_sent_to_slack
+    assert_no_success_notification_sent
   end
 
   def test_run_success_spec
@@ -57,10 +70,6 @@ class FunctionalTest < Minitest::Test
       monitor.monitor 'spec/success_spec.rb', ENV['SLACK_WEBHOOK']
     }
     assert_no_notification_sent_to_slack
-    assert_equal 1, success_notifications.size
-    success_notification = JSON.parse( RestClient.get "https://api.runscope.com/buckets/32dq2c18qk24/messages/#{success_notifications.first['uuid']}", {:Authorization => 'Bearer 05d16442-31df-4f23-947d-9a5eec4f0525'} )
-    body = JSON.parse success_notification["data"]["request"]["body"]
-    assert_equal("all monitoring tests passed", body["data"])
   end
 
 
